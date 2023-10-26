8d4eb59e55a40845d0605b29529923471355172a
QuincieBird <114311657+QuincieBird@users.noreply.github.com>
2023-10-26 10:11:33 -0700

    Update objects.js

--- objects.js
+++ objects.js
@@ -8,29 +8,31 @@ class PlayerData {
   }
 
 // character Object
-class character = {
-    constructor(){
-        name
-        level: 1,
-        gold: 100,
-        currentHealth: 100,
-        maxHealth: 100,
-        currentMana: 50,
-        maxMana: 50,
-        currentXP: 0,
-        maxXP: 100,
-        knownSkills: [], // An array to store skill objects
-        heldItems: [], // An array to store item objects
-        maxCarryCapacity: 100, // Max weight character can carry
-        currentWeightHeld: 0, // Current weight held
-        attackDamage: 10,
-        autoAttackSpeed: 1, // Attacks per second
-        dodgeChance: 5, // Percentage chance of dodging an attack
-        defense: 10,
-        floorsDescended: 0,
-        characterName: "CharacterName",
-        luck: 1, // A variable for character's luck
-};
+class Character {
+  constructor(name, level, gold, maxHealth, maxMana, maxXP, maxCarryCapacity) {
+    this.name = name;
+    this.level = level;
+    this.gold = gold;
+    this.currentHealth = maxHealth;
+    this.maxHealth = maxHealth;
+    this.currentMana = maxMana;
+    this.maxMana = maxMana;
+    this.currentXP = 0;
+    this.maxXP = maxXP; // Corrected from maxHP
+    this.knownSkills = []; // An array to store skill objects
+    this.heldItems = []; // An array to store item objects
+    this.maxCarryCapacity = maxCarryCapacity; // Max weight character can carry
+    this.currentWeightHeld = 0; // Current weight held
+    this.attackDamage = 10;
+    this.autoAttackSpeed = 1; // Attacks per second
+    this.dodgeChance = 5; // Percentage chance of dodging an attack
+    this.defense = 10;
+    this.floorsDescended = 0;
+    this.luck = 1; // A variable for character's luck
+    this.companions = []; // Array to hold companion objects
+    this.skillPoints = 0;
+  }
+}
 
 // monster Object
 function Monster(floor) {
@@ -45,9 +47,15 @@ function Monster(floor) {
   this.monsterName = "MonsterName"; // You can set a name or generate one based on the floor
   this.knownSkill = null; // A skill object that the monster can use
 }
+  
 
 // companion Object (add companion-related properties here)
-
+function Companion(name, hp, attackDamage) {
+  this.name = name;
+  this.hp = hp;
+  this.attackDamage = attackDamage;
+}
+  
 // skill Object (add skill-related properties here)
 
 // item Object (add item-related properties here)
